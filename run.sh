3472758495ce90f562b56efb5748dd4939c9a3f1
leili <leili.tavabi@gmail.com>
2019-06-23 13:47:26 -0700

    Adding run.sh

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ python train_models.py --feature_dim 100 --feature_type BoW_mfcc --max_sequence_
 # Subsampling with a 1/4 ratio for VGG & DS_VGG and 1/2 ratio for ResNet & DS_densenet
 # Cropping 20 minutes according to the new frame rate
 
-python train_models.py --feature_dim 4096 --feature_type DS_VGG --max_sequence_length 9000 --modality vision --learning_rate 0.0001 --logger_name DS_VGG.log
-python train_models.py --feature_dim 1920 --feature_type DS_densenet --max_sequence_length 18000 --modality vision --learning_rate 0.0005 --logger_name DS_densenet.log
+python train_models.py --feature_dim 4096 --feature_type DS_VGG --max_sequence_length 9000 --modality speech --learning_rate 0.0001 --logger_name DS_VGG.log
+python train_models.py --feature_dim 1920 --feature_type DS_densenet --max_sequence_length 18000 --modality speech --learning_rate 0.0005 --logger_name DS_densenet.log
 python train_models.py --feature_dim 2048 --feature_type ResNet --max_sequence_length 18000 --modality vision --learning_rate 0.0001 --logger_name ResNet.log
 python train_models.py --feature_dim 4096 --feature_type VGG --max_sequence_length 9000 --modality vision --learning_rate 0.001 --logger_name VGG.log
 
